4c8e5dead8e51dcdeef4d9b3643b4e0831376de0
Jaes98 <marcus.jaes@gmail.com>
2024-03-13 11:10:55 +0100

    CK-36 omdb api sat op, hardcoded apikey lige nu. Mangler at init movies på startup.

--- src/main/java/dat3/kino/controllers/MovieController.java
+++ src/main/java/dat3/kino/controllers/MovieController.java
@@ -1,5 +1,6 @@
 package dat3.kino.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import dat3.kino.entity.Movie;
 import dat3.kino.service.CinemaService;
 import dat3.kino.service.MovieService;
@@ -20,6 +21,16 @@ public class MovieController {
     @GetMapping
     public List<Movie> getAll(){
         return movieService.findAll();
+    }
+
+    @RequestMapping("/imdbid/{imdbId}")
+    public Movie getMovie(@PathVariable String imdbId) {
+        return movieService.getMovieByImdbId(imdbId);
+    }
+
+    @PostMapping("/{imdbId}")
+    public Movie addMovie(@PathVariable String imdbId) throws JsonProcessingException {
+        return movieService.addMovie(imdbId);
     }
 
     @GetMapping(path = "{id}")
--- src/main/java/dat3/kino/repository/MovieRepository.java
+++ src/main/java/dat3/kino/repository/MovieRepository.java
@@ -3,5 +3,8 @@ package dat3.kino.repository;
 import dat3.kino.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    Optional<Movie> findByImdbID(String imdbId);
 }
