7b306cc4138a913399355dd2f41b01d51b1ed322
buan0001 <123996422+buan0001@users.noreply.github.com>
2024-03-13 10:06:06 +0100

    CK-20 findAllByShowingId

--- src/main/java/dat3/kino/repository/ReservationRepository.java
+++ src/main/java/dat3/kino/repository/ReservationRepository.java
@@ -3,5 +3,9 @@ package dat3.kino.repository;
 import dat3.kino.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    public List<Reservation> findAllByShowingId(Long id);
 }
